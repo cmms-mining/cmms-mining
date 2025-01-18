58dbe64501c354e40b603ee1fb775cd3cc1ea777
cmms-mining <cmms.mining@gmail.com>
2025-01-18 23:25:27 +0500

    Добавление страницы списка проверки систем пожаротушения

--- source/apps/firefighting/models.py
+++ source/apps/firefighting/models.py
@@ -1,29 +1,13 @@
 from django.db import models
 
 
-class FirefightingSystem(models.Model):
-    equipment = models.OneToOneField(
-        to='equipments.Equipment',
-        related_name='firefighting',
-        verbose_name='Оборудование',
-        on_delete=models.CASCADE,
-        )
-    requires_check = models.BooleanField(verbose_name='Требует проверки', default=True)
-
-    class Meta:
-        verbose_name = 'Система пожаротушения'
-        verbose_name_plural = 'Системы пожаротушения'
-
-    def __str__(self):
-        return f'Система пожаротушения {self.equipment.number}'
-
-
 def attach_firefighting_check_upload_path(instance, filename):
     return f'firefighting/{instance.firefighting_system.equipment.number}' \
            f'/{instance.date.strftime("%d-%m-%Y")}/{filename}'
 
 
 class FirefightingCheck(models.Model):
+    """Проверки систем пожаротушения"""
     firefighting_system = models.ForeignKey(
         to='firefighting.FirefightingSystem',
         related_name='firefighting_checks',
@@ -37,16 +21,35 @@ class FirefightingCheck(models.Model):
         ('Неисправна', 'Неисправна'),
         ('Неустановлена', 'Неустановлена'),
     )
-    state = models.CharField(
-        verbose_name='Состояние',
-        max_length=20,
-        choices=STATE_CHOICES,
-    )
+    state = models.CharField(verbose_name='Состояние', max_length=20, choices=STATE_CHOICES)
     note = models.CharField(verbose_name='Примечание', max_length=100, blank=True, null=True)
 
     class Meta:
         verbose_name = 'Проверка системы пожаротушения'
         verbose_name_plural = 'Проверки систем пожаротушения'
+        ordering = ['-date']
 
     def __str__(self):
         return f'Проверка системы пожаротушения {self.firefighting_system.equipment.number}'
+
+
+class FirefightingSystem(models.Model):
+    """Установленные системы пожаротушения на оборудовании"""
+    equipment = models.OneToOneField(
+        to='equipments.Equipment',
+        related_name='firefighting',
+        verbose_name='Оборудование',
+        on_delete=models.CASCADE,
+        )
+    requires_check = models.BooleanField(verbose_name='Требует проверки', default=True)
+
+    class Meta:
+        verbose_name = 'Система пожаротушения'
+        verbose_name_plural = 'Системы пожаротушения'
+
+    def get_check(self) -> FirefightingCheck | None:
+        last_check = FirefightingCheck.objects.filter(firefighting_system=self).first()
+        return last_check
+
+    def __str__(self):
+        return f'Система пожаротушения {self.equipment.number}'
--- source/apps/firefighting/views.py
+++ source/apps/firefighting/views.py
@@ -1,5 +1,9 @@
-from django.views.generic import TemplateView
+from django.views.generic import ListView
+
+from .models import FirefightingSystem
 
 
-class FirefightingsListView(TemplateView):
+class FirefightingsListView(ListView):
+    context_object_name = 'firefighting_systems'
     template_name = 'firefighting/firefightings_list.html'
+    model = FirefightingSystem
