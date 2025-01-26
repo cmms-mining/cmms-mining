91cf061d2a45dd7c8613d0964a80dc08a63519c8
cmms-mining <cmms.mining@gmail.com>
2025-01-26 11:17:50 +0500

    Импорт наработки оборудования из гуглдиска

--- source/apps/importer/google.py
+++ source/apps/importer/google.py
@@ -1,5 +1,6 @@
 import os
 import pickle
+from datetime import datetime
 
 from google.auth.transport.requests import Request
 
@@ -7,11 +8,17 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 
 from googleapiclient.discovery import Resource, build
 
+import pandas as pd
+
+from apps.equipments.models import Equipment, EquipmetRunningTime
+
+from .files_id import FILE_ID
+
 
 PATH = os.path.dirname(os.path.abspath(__file__))
 CLIENT_SECRET_FILE = os.path.join(PATH, "client_secret.json")
-API_NAME = "drive"
-API_VERSION = "v3"
+API_NAME = "sheets"
+API_VERSION = "v4"
 SCOPES = ["https://www.googleapis.com/auth/drive"]
 
 
@@ -44,4 +51,28 @@ def get_cred_from_pickle():
 
 
 def connect_google_drive():
-    create_service()
+    service = create_service()
+    sheet = service.spreadsheets()
+    result = sheet.values().get(spreadsheetId=FILE_ID, range='Лист3!A:C').execute()
+    values = result.get('values', [])
+
+    df = pd.DataFrame(values)
+
+    df = df[1:]  # Убираем первую строку из данных
+
+    df.columns = ['date', 'equipment_number', 'running_time']
+    df.replace('', pd.NA, inplace=True)
+    df = df.dropna(subset=['equipment_number'])
+
+    for _index, row in df.iterrows():
+        equipment_number = row.to_dict().get('equipment_number')
+        if Equipment.objects.filter(number=equipment_number).exists():
+            equipment = Equipment.objects.get(number=equipment_number)
+            date_str: str = row.to_dict().get('date')
+            date = datetime.strptime(date_str, "%d.%m.%Y").date()
+            running_time = row.to_dict().get('running_time')
+            EquipmetRunningTime.objects.get_or_create(
+                equipment=equipment,
+                date=date,
+                defaults={'running_time': running_time},
+                )
